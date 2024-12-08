5e237e7284aa51cc25a6cdc2f74c324cd12f564b
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-12-08 21:12:37 +0100

    change workout urls for stats

--- workouts/urls.py
+++ workouts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.WorkoutList.as_view(), name='workout-list'),
     path('<int:pk>/', views.WorkoutDetail.as_view(), name='workout-detail'),
-    path('statistics/<int:user_id>/', views.workout_statistics, name='workout-statistics'),
+    path('statistics/', views.workout_statistics, name='workout-statistics'),
 ]

d2392b0bd8a4a77c51f0494dbcca0479c434d64d
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-12-09 21:44:32 +0100

    add admin urls route

--- fitapi/urls.py
+++ fitapi/urls.py
@@ -4,6 +4,7 @@ from .views import root_route, logout_route
 
 urlpatterns = [
     path('', root_route, name='root'),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('logout/', logout_route),
     path('profiles/', include('profiles.urls')),

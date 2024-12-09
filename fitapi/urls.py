a80de1cd2910d6f85f49f4171bfb9435ee691c6d
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-12-09 20:52:08 +0100

    update urls

--- fitapi/urls.py
+++ fitapi/urls.py
@@ -2,21 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
 
-
 urlpatterns = [
     path('', root_route, name='root'),
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include(
-        'dj_rest_auth.registration.urls')),
-
-    # API endpoints
-    path('api/workouts/', include('workouts.urls')),
-    path('api/posts/', include('workoutposts.urls')),
-    path('api/profiles/', include('profiles.urls')),
-    path('api/likes/', include('likes.urls')),
-    path('api/comments/', include('comments.urls')),
-    path('api/followers/', include('followers.urls')),
+    path('logout/', logout_route),
+    path('profiles/', include('profiles.urls')),
+    path('workouts/', include('workouts.urls')),
+    path('posts/', include('workoutposts.urls')),
+    path('likes/', include('likes.urls')),
+    path('comments/', include('comments.urls')),
+    path('followers/', include('followers.urls')),
 ]
--- workoutposts/urls.py
+++ workoutposts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from workoutposts import views
 
 urlpatterns = [
-    path('posts/', views.WorkoutPostList.as_view(), name='workoutpost-list'),
+    path('', views.WorkoutPostList.as_view(), name='workoutpost-list'),
 ]

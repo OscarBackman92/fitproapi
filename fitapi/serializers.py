f8fda3e7ade4656b6db07e1f791fa4e0deeeef3e
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-12-10 22:15:46 +0100

    update fitapi serializer

--- fitapi/serializers.py
+++ fitapi/serializers.py
@@ -2,11 +2,18 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
-    username = serializers.ReadOnlyField()
-    
+    pk = serializers.ReadOnlyField(source='id')
+    profile = serializers.SerializerMethodField()
+
+    def get_profile(self, obj):
+        if hasattr(obj, 'profile'):
+            return {
+                'profile_id': obj.profile.id,
+                'profile_image': obj.profile.image.url if obj.profile.image else None,
+            }
+        return None
+
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image', 'username'
+            'pk', 'username', 'profile'
         )

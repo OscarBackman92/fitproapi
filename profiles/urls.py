26717056705d6f675c9c8b305384b0040bd1a7f2
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-12-10 19:58:50 +0100

    update profile urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ProfileList.as_view(), name='profile-list'),
     path('current/', views.CurrentUserProfile.as_view(), name='current-user-profile'),
-    path('<int:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
-    path('<int:pk>/statistics/', views.profile_statistics, name='profile-statistics'),
+    path('<str:owner>/', views.ProfileDetail.as_view(), name='profile-detail'),
+    path('<str:owner>/statistics/', views.profile_statistics, name='profile-statistics'),
 ]

4fd7b417b333ced04b5e81921308c3bf8998b05e
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-12-10 22:37:36 +0100

    add profile.id to fitapi serializer

--- fitapi/serializers.py
+++ fitapi/serializers.py
@@ -4,9 +4,6 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
-    username = serializers.ReadOnlyField()
-    
+
     class Meta(UserDetailsSerializer.Meta):
-        fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image', 'username'
-        )
+        fields = UserDetailsSerializer.Meta.fields + ('profile_id', 'profile_image')
